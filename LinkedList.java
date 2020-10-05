f257ada8be9211fb0090277d9bdf6037bcfb40bc
dli0036 <dliulic@gmail.com>
2020-10-05 18:37:43 -0400

    some init msg

---
+++ LinkedList.java
@@ -0,0 +1,19 @@
+/* Linked List (empty), shows structure of classes and initial members */
+
+class Node  {
+   public int value;
+   public Node next;  
+  
+   // Constructor
+   public Node(int i) { /* blank for now */ }
+  
+   // Method
+   public void displayLink() { /* blank for now */ }
+}
+
+class LinkedList {
+  // uses Node class
+  private Node first;        // private link to the first element 
+  public void LinkedList(){} // constructor sets first to null
+  public boolean isEmpty(){} // returns true if list is empty
+}
